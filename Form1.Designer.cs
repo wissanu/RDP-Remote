775d6edb19c63ce85500bbc39488afa8d0911fdb
Wissanu <thungrut@gmail.com>
2020-09-02 03:39:59 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace RDP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rdpCtl = new AxMSTSCLib.AxMsTscAxNotSafeForScripting();
+            this.button1 = new System.Windows.Forms.Button();
+            this.IP = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.rdpCtl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rdpCtl
+            // 
+            this.rdpCtl.Enabled = true;
+            this.rdpCtl.Location = new System.Drawing.Point(12, 12);
+            this.rdpCtl.Name = "rdpCtl";
+            this.rdpCtl.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("rdpCtl.OcxState")));
+            this.rdpCtl.Size = new System.Drawing.Size(984, 671);
+            this.rdpCtl.TabIndex = 0;
+            this.rdpCtl.OnConnecting += new System.EventHandler(this.axMsTscAxNotSafeForScripting1_OnConnecting);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(544, 693);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "LOG IN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // IP
+            // 
+            this.IP.AutoSize = true;
+            this.IP.Location = new System.Drawing.Point(14, 699);
+            this.IP.Name = "IP";
+            this.IP.Size = new System.Drawing.Size(17, 13);
+            this.IP.TabIndex = 2;
+            this.IP.Text = "IP";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 696);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 699);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "User";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(207, 697);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(349, 699);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Pass";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(385, 696);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(661, 693);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 26);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "LOG OUT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.IP);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rdpCtl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.rdpCtl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxMSTSCLib.AxMsTscAxNotSafeForScripting rdpCtl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label IP;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RDP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void loop()
+        {
+            int count = 0;
+            for (int i = 0; i < 10000; i++)
+            {
+                count = count + 1;
+                count = count + 2;
+            }
+        }
+
+        private int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private void Loop()
+        {
+            return;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Loop();
+            rdpCtl.Server = textBox1.Text;
+            rdpCtl.UserName = textBox2.Text;
+            MSTSCLib.IMsTscNonScriptable mstscScript = (MSTSCLib.IMsTscNonScriptable)rdpCtl.GetOcx();
+            mstscScript.ClearTextPassword = textBox3.Text;
+            rdpCtl.Connect();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (rdpCtl.Connected.ToString() == "1")
+            rdpCtl.Disconnect();
+        }
+
+        
+    }
+}
